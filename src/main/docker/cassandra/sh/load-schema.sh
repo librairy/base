62f1b3dc11b5eaa2a08f438b772668ba405795d3
Carlos Badenes <cbadenes@gmail.com>
2016-09-05 14:35:26 +0200

    clear log messages

--- src/main/docker/cassandra/sh/load-schema.sh
+++ src/main/docker/cassandra/sh/load-schema.sh
@@ -2,21 +2,16 @@
 
 while : ;do
   sleep 5;
-  echo "=======> checking cassandra status"
+  echo "checking cassandra status"
   # Get the status of this machine from the Cassandra nodetool
   #STATUS=`nodetool status | grep 'UN' | awk '{print $1}'`
   STATUS=`nodetool statusthrift`
-  echo $STATUS
   # Once the status is Up and Normal, then we are ready
   #if [ $STATUS = "UN" ]; then
-  if [ $STATUS = "running" ]; then
-        echo "===================================================="
-        echo "loading librairy schema"
-        echo "===================================================="
+  if [ "$STATUS" = "running" ]; then
+        echo "loading librairy schema .."
         cqlsh -f /tmp/librairy-schema.cql
-        echo "===================================================="
-        echo "librairy schema loaded successfully"
-        echo "===================================================="
+        echo "librairy schema loaded"
         break
   fi
 
--- src/main/docker/neo4j/sh/create_constraints.sh
+++ src/main/docker/neo4j/sh/create_constraints.sh
@@ -1,22 +1,14 @@
 #!/usr/bin/env bash
 
-/var/lib/neo4j/bin/neo4j-shell -c < /tmp/grapdb-constraints.cql
-
 while : ;do
   sleep 5;
-  echo "=======> checking neo4j status"
-  # Get the status of this machine from the Cassandra nodetool
-  #STATUS=`nodetool status | grep 'UN' | awk '{print $1}'`
+  echo "checking neo4j status"
   STATUS=`/var/lib/neo4j/bin/neo4j-shell -c "schema" | tail -1`
   # Once the status is Up and Normal, then we are ready
   if [ "$STATUS" = "No constraints" ]; then
-        echo "===================================================="
-        echo "loading constraints"
-        echo "===================================================="
+        echo "loading node and relation constraints .."
         /var/lib/neo4j/bin/neo4j-shell -file /tmp/graphdb-constraints.cql
-        echo "===================================================="
-        echo "librairy constraints loaded successfully"
-        echo "===================================================="
+        echo "librairy constraints loaded"
         break
   fi
 
